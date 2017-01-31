a39422038c9057b6e62b6c23b4a37c664cad9129
Thomas Klausner <domm@plix.at>
2017-01-31 15:44:07 +0100

    bump version

--- lib/DBIx/Class/InflateColumn/JSON2Object/Role/Storable.pm
+++ lib/DBIx/Class/InflateColumn/JSON2Object/Role/Storable.pm
@@ -66,3 +66,30 @@ sub package {
 }
 
 1;
+
+__END__
+
+=pod
+
+=encoding UTF-8
+
+=head1 NAME
+
+DBIx::Class::InflateColumn::JSON2Object::Role::Storable - simplified MooseX::Storage clone with enhanced JSON boolean handling
+
+=head1 VERSION
+
+version 0.900
+
+=head1 AUTHOR
+
+Thomas Klausner <domm@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2017 by Thomas Klausner.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
--- lib/DBIx/Class/InflateColumn/JSON2Object/Trait/NoSerialize.pm
+++ lib/DBIx/Class/InflateColumn/JSON2Object/Trait/NoSerialize.pm
@@ -8,3 +8,30 @@ package Moose::Meta::Attribute::Custom::Trait::NoSerialize {
 }
 
 1;
+
+__END__
+
+=pod
+
+=encoding UTF-8
+
+=head1 NAME
+
+DBIx::Class::InflateColumn::JSON2Object::Trait::NoSerialize - NoSerialize trait for attributes
+
+=head1 VERSION
+
+version 0.900
+
+=head1 AUTHOR
+
+Thomas Klausner <domm@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2017 by Thomas Klausner.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
